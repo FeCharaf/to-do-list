8b257c17249f479bef9651f6f68a76cf5380b114
FeCharaf <felipecharaf@gmail.com>
2023-08-24 15:38:50 -0300

    Adicionando filtros

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -30,7 +30,8 @@ function App() {
 
   const [search, setSearch] = useState("");
 
-  const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -63,13 +64,15 @@ function App() {
   return (
   <div className="app">
     <h1>Lista de Tarefas</h1>
-    <Search search={search} setSearch={setSearch}/>
-    <Filter />
+    <Search search={search} setSearch={setSearch} />
+    <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
     <div className="todo-list">
       {todos
+        .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
         .filter((todo) =>
         todo.text.toLowerCase().includes(search.toLowerCase())
         )
+        .sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
         .map((todo) => (
         <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
       ))}
